be0b8bbd7750c80406be6c06fcf159918711bb2b
tuan anh <97lynk@gmail.com>
2019-12-06 00:09:32 +0700

    fix bug cors

--- src/main/java/vn/edu/hcmute/grab/config/oauth2/CorsFilter.java
+++ src/main/java/vn/edu/hcmute/grab/config/oauth2/CorsFilter.java
@@ -16,7 +16,7 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         final HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*,https://grabtho.web.app,https://management-grabtho.web.app");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, PATCH, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, X-Requested-With");
         response.setHeader("Access-Control-Max-Age", "3600");
